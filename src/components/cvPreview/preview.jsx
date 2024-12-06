e3e57e568eb99ef81c5a634a5bc91658af771e46
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-12-07 01:12:02 +0530

    initial preview of cv

---
+++ src/components/cvPreview/preview.jsx
@@ -0,0 +1,34 @@
+export default function Preview(){
+    return(
+        <div id="cvPreview">
+            <div className="info">
+                <div className="info1">
+                    <div className="name">Sarthak</div>
+                    <div className="title">mr.</div>
+                </div>
+                <div className="info2">
+                <div className="box">
+                <span className="material-symbols-outlined">location_on</span>
+                <div>uuu</div>
+                </div>
+                <div className="box">
+                <span className="material-symbols-outlined">call</span>
+                <div></div>
+                </div>
+                <div className="box">
+                <span className="material-symbols-outlined">mail</span>
+                <div></div>
+                </div>
+                </div>
+            </div>
+            <hr></hr>
+            <div className="exp">
+                <h2>Experience</h2>
+
+            </div>
+            <div className="edu">
+                <h2>Education</h2>
+            </div>
+        </div> 
+    )
+}
