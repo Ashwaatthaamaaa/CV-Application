ac3d12fc3d60f95c1433741770e3ce77cda9a6d1
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-12-08 03:43:24 +0530

    create preview components for education and work exp. section

---
+++ src/components/cvPreview/workExPreview.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import './preview.css'
+export default function WorkExPreview({exp}){
+    return(
+        <div className="exp">
+            <div className="first">
+                <div className="position"><strong>{exp.position}</strong></div>
+                <div className="company">
+                    <strong>{exp.position}</strong>|{exp.startDate}-{exp.endDate}
+                </div>
+            </div>
+            <div className="second">
+                {exp.description}
+            </div>
+        </div>
+    )
+}
