ab6cda79ed3d383aa8956df8c74ddb26089f04bb
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-12-08 04:32:03 +0530

    remove useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './header'
+import Content from './content'
 
 function App() {
   const [count, setCount] = useState(0)
